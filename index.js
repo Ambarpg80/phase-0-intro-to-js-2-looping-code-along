d5ef982aa58e7a3e63c7130b20a121c6c817b319
Ambar <Ambarpg80@gmail.com>
2022-09-08 21:21:05 -0400

    commit tree 3b483c612bb132fe5101aa17321e40e449533e73

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name,eventName){
+    const messages = [];
+    for (let i = 0 ; i < name.length ; i++){
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages;
+}
+writeCards()
+
+function countDown(number){
+    let i = 0;
+    while(i <= number){ 
+        console.log(number--);
+    }
+}
+countDown(10);
